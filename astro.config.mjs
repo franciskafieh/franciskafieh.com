50329430cb6d73f27d23576e48ecd137777d17e0
Francis Kafieh <kafieh.francis@gmail.com>
2022-08-13 01:07:38 -0400

    added test email form

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import robotsTxt from 'astro-robots-txt';
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import fonts from 'astro-fonts-next';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://franciskafieh.com",
-  integrations: [svelte(), partytown(), robotsTxt(), sitemap(), compress()],
+  integrations: [
+    svelte(), 
+    partytown(), 
+    robotsTxt(), 
+    sitemap(), 
+    compress(),
+    fonts({ url: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Montserrat:wght@400;700;800&display=swap' }),
+  ],
   experimental: {
     integrations: true
   }
