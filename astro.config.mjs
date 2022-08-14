5d4b6677cee439812f47c49c0319cd755da15379
Francis Kafieh <kafieh.francis@gmail.com>
2022-08-14 19:13:22 -0400

    added read time util

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,31 @@ import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import fonts from 'astro-fonts-next';
+import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+
+// import readingTime from 'remark-reading-time';
+// import readingMdxTime from 'remark-reading-time';
+import { getReadTime } from './src/lib/remark-read-time.mjs';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://franciskafieh.com",
   integrations: [
-    partytown(), 
-    robotsTxt(), 
-    sitemap(), 
-    compress(),
-    fonts({ url: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Montserrat:wght@400;700;800&display=swap' }),
+    partytown(), robotsTxt(), sitemap(), compress(),
+    mdx({
+      remarkPlugins: { extends: [getReadTime] },
+    }), 
+    fonts({
+      url: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Montserrat:wght@400;700;800&display=swap'
+    }),
+    tailwind()
   ],
   experimental: {
-    integrations: true
+    integrations: true,
+  },
+  markdown: {
+    syntaxHighlight: 'prism',
   }
 });
