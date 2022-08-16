bf405a2bc445f6b209e360cf020478c9579267be
Francis Kafieh <kafieh.francis@gmail.com>
2022-08-15 23:04:10 -0400

    push to test analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import fonts from 'astro-fonts-next';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 
-// import readingTime from 'remark-reading-time';
-// import readingMdxTime from 'remark-reading-time';
 import { getReadTime } from './src/lib/remark-read-time.mjs';
+import remarkCapitalize from 'remark-capitalize';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeAddClasses from 'rehype-add-classes';
+import { h } from 'hastscript';
+import { rehypePrismCommon } from 'rehype-prism-plus';
 
 
 // https://astro.build/config
@@ -18,8 +21,17 @@ export default defineConfig({
   integrations: [
     partytown(), robotsTxt(), sitemap(), compress(),
     mdx({
-      remarkPlugins: { extends: [getReadTime] },
-    }), 
+      remarkPlugins: { extends: [getReadTime, remarkCapitalize] },
+      rehypePlugins: { extends: [
+        [rehypeAutolinkHeadings, {      
+          content(_node) {
+            return [h('span.group-hover:text-red-400', '#')]
+          }
+        }],
+        [rehypeAddClasses, {h1: 'group'}],
+        [rehypePrismCommon, {showLineNumbers: true, ignoreMissing: true}]
+      ]},
+    }),
     fonts({
       url: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Montserrat:wght@400;700;800&display=swap'
     }),
@@ -29,6 +41,7 @@ export default defineConfig({
     integrations: true,
   },
   markdown: {
-    syntaxHighlight: 'prism',
+    // using rehype-prism-plus
+    syntaxHighlight: false,
   }
 });
