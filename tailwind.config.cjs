e3f1d4e853f97a5b75c5500a89738bb8e54e736c
Francis Kafieh <kafieh.francis@gmail.com>
2022-08-18 00:13:32 -0400

    style changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const themeSwapper = require('tailwindcss-theme-swapper');
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,mdx,ts}"],
   theme: {
     fontFamily: {
       sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
@@ -19,5 +20,33 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    themeSwapper({
+			themes: [
+			  {
+				name: 'base',
+				selectors: [':root'],
+				theme: {
+				  colors: {
+					primary: '#0C101E',
+					accent: '#704BE0',
+					background: '#FCF5FF'
+				  },
+				},
+			  },
+			  {
+				name: 'dark',
+				selectors: ['.dark'],
+				theme: {
+				  colors: {
+					primary: '#FCF5FF',
+					accent: '#FFD500',
+					background: '#0C101E'
+				  },
+				},
+			  },
+			],
+		}),
+  ],
 };
